95c37e722a820fccc84b89d3a01dc553402911dc
oaglazunova <oaglazunova@gmail.com>
2015-04-21 17:20:44 +0300

    ...

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
 
+	
+	
   grunt.initConfig({
 
     less: {
@@ -47,31 +49,7 @@ module.exports = function(grunt) {
       },
     },
 	
-	// Run this task with the  grunt imagemin  command.
-	imagemin: {                          // Task
-    static: {                          // Target
-      options: {                       // Target options
-        optimizationLevel: 3,
-        svgoPlugins: [{ removeViewBox: false }],
-        use: [mozjpeg()]
-      },
-      files: {                         // Dictionary of files
-        'dist/img.png': 'src/img.png', // 'destination': 'source'
-        'dist/img.jpg': 'src/img.jpg',
-        'dist/img.gif': 'src/img.gif'
-      }
-    },
-    dynamic: {                         // Another target
-      files: [{
-        expand: true,                  // Enable dynamic expansion
-        cwd: 'src/',                   // Src matches are relative to this path
-        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-        dest: 'dist/'                  // Destination path prefix
-      }]
-    },
-  }
-  // Run this task with the  grunt imagemin  command.
-  
+
   
 
   });
@@ -80,7 +58,6 @@ module.exports = function(grunt) {
     'less',
     'autoprefixer',
     'cmq',
-	'imagemin',	
     'watch',
   ]);
 
@@ -89,3 +66,5 @@ module.exports = function(grunt) {
     'autoprefixer',
     'cmq',
   ]);
+  
+};
