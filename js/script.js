27ac67c09e1a7f91a9bd4b95a5282674b16268d3
oaglazunova <oaglazunova@gmail.com>
2015-05-28 20:41:52 +0300

    Responsive layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+var toggler = document.getElementById('toggler');
+var menu = document.getElementById('menu');
+    toggler.onclick = function (e) {
+     e.preventDefault();
+     toggler.classList.toggle('menu__toggler--close');
+	 menu.classList.toggle('menu--vertical');
+    } 
 /*
   -------------------------------------------------------------------------
 		      JavaScript Form Validator (gen_validatorv31.js)
